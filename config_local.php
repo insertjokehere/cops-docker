4112367afe96202b8074f3f886b2cc1c4da5f292
William Hughes <williamh@catalyst.net.nz>
2016-08-08 14:55:57 +1200

    Bad config file

--- config_local.php
+++ config_local.php
@@ -7,7 +7,7 @@
      * containing all the formats.
      * BEWARE : it has to end with a /
      */
-    $config['calibre_directory'] = '/tmp/metadata';
+    $config['calibre_directory'] = '/tmp/metadata/';
     
     /*
      * Catalog's title
